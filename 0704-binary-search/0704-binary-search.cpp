57f005720846a988262db5b64c25760be4ffda3b
Ahmad ali <ahmadali786575@gmail.com>
2026-08-15 00:17:10 +0530

    Time: 0 ms (100%), Space: 31.3 MB (46.79%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -6,7 +6,7 @@ public:
         
         while(start<= end){
             
-            int mid = (start+end)/2;
+            int mid = start + (end - start)/2;
             if(nums[mid]==target){
                 return mid;
             }
